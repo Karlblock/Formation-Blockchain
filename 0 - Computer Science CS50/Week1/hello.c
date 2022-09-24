76a01158c20d8687945fa37eec4d1ed8e2ba3c2d
root <root@DESKTOP-QDUVMKP.localdomain>
2022-09-24 12:39:20 +0200

    Road To BC Dev

--- 0 - Computer Science CS50/Week1/hello.c
+++ 0 - Computer Science CS50/Week1/hello.c
@@ -1,6 +1,8 @@
+#include <cs50.h>
 #include <stdio.h>
 
 int main(void)
 {
-    printf("hello world ! \n");
+    string answer = get_string("What's your name? ");
+    printf("hello, %s\n", answer);
 }
---
+++ 0 - Computer Science CS50/Week1/mario.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void)
+{
+    for (int i = 0; i < 4; i++)
+    {
+        printf("?");
+    }
+    printf("\n");
+}
